a87fd64d588d2d7a57f4efa8a8bc6281469dc8d8
Josh Johnson <JoshJ@highjump.com>
2018-01-04 15:55:44 -0500

    Initial Commit

---
+++ Tc.Psg.Logging/Extensions/ILoggerFactoryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+
+using Tc.Psg.Logging;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static partial class ILoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddPsgLogging(this ILoggerFactory loggerFactory, IConfiguration configuration)
+        {
+            PsgLoggingOptions options = configuration.Get<PsgLoggingOptions>();
+
+            return loggerFactory.AddPsgLogging(options);
+        }
+
+        public static ILoggerFactory AddPsgLogging(this ILoggerFactory loggerFactory, PsgLoggingOptions options)
+        {
+            LoggingLevelSwitch loggingLevelSwitch = new LoggingLevelSwitch();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithEnvironmentUserName()
+                .Enrich.WithMachineName()
+                .MinimumLevel.ControlledBy(loggingLevelSwitch)
+                .WriteTo.Seq(options.SeqServerUrl, apiKey: options.SeqApiKey, controlLevelSwitch: loggingLevelSwitch)
+                .CreateLogger();
+
+            return loggerFactory.AddSerilog();
+        }
+    }
+}
---
+++ Tc.Psg.Logging/PsgLoggingOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Tc.Psg.Logging
+{
+    public class PsgLoggingOptions
+    {
+        public PsgLoggingOptions()
+        {
+            SeqServerUrl = "http://seq.tcpsg.net:80";
+        }
+
+        public string SeqApiKey { get; set; }
+        public string SeqServerUrl { get; set; }
+    }
+}
